499d10580155552afd4c6a642b67ce2670a99f03
Altneo <charismaticc27@gmail.com>
2021-12-28 14:51:03 +0530

    one stage closer to the work

--- Reverse World Clock.py
+++ Reverse World Clock.py
@@ -3,13 +3,17 @@ from datetime import date
 from datetime import datetime
 #Show current time
 today=date.today()
-time=datetime.now()
+timethen=datetime.now()
 print("today's date is ",today)
-print('Time is ',time)
-print("2018-06-29 08:15:27")
+print('Time is ',timethen)
+print("A sample date to input")
+print("2021-12-28 09:17:27")
 entertime= input("Enter the time of the day (yyyy-mm-dd hh:mm:ss)")
 timetaken=datetime.strptime(entertime, '%Y-%m-%d %H:%M:%S')
 print("Date: ", timetaken.date())
 print("Time: ", timetaken.time())
-#
-
+if str(timetaken) > str(timethen):
+    print('Time Difference=', str(timetaken -timethen))
+else:
+    print('Time Difference=', str(timethen-timetaken))
+    print("So we know that ")
