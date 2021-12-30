7470df979db1b605f6fe36246d8885a23a545155
Altneo <charismaticc27@gmail.com>
2021-12-30 09:41:57 +0530

    Completed the project with just one room for improvement

--- Reverse World Clock.py
+++ Reverse World Clock.py
@@ -1,6 +1,13 @@
 #Method 2
-from datetime import date
-from datetime import datetime
+import json
+import re
+from datetime import date, datetime, timezone
+import pytz
+import timezonefinder
+tzf=timezonefinder.TimezoneFinder()
+
+from urllib.request import urlopen
+
 #Show current time
 today=date.today()
 timethen=datetime.now().replace(microsecond= 0)
@@ -26,4 +33,37 @@ print('Total difference in minutes: ', timediff)
 timedifflist=timediff.split(":")
 #print(timedifflist)
 totalmins=(int(timedifflist[0])*60)+int(timedifflist[1])
-print(totalmins)
+#print(totalmins)
+
+
+## Now for the Part 2 of the project.
+## Part 2 a)
+## Getting Ip of the user.
+url = 'http://ipinfo.io/json'
+response = urlopen(url)
+data = json.load(response)
+
+IP=data['ip']
+org=data['org']
+city = data['city']
+country=data['country']
+region=data['region']
+
+#print(IP)
+
+
+#getting co-ordinates from ip
+
+url= 'http://ip-api.com/json/'+IP
+response= urlopen(url)
+data= json.load(response)
+longitude=int(data.get('lon'))
+#print(longitude)
+if ahead ==1:
+    longitude=longitude+(totalmins/4)
+    timezone=tzf.certain_timezone_at(lat=int(data.get('lat')),lng=longitude)
+    print(timezone)
+else :
+    longitude=longitude-(totalmins/4)
+    timezone=tzf.certain_timezone_at(lat=int(data.get('lat')),lng=longitude)
+    print(timezone)
